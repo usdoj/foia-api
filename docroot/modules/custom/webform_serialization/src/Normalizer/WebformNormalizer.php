38b15753a150fee29afcf71e436fdda039c0b961
Brian Reese <Brian_R_Reese@oa.eop.gov>
2017-06-20 12:05:32 -0400

    DOJ-1: Parse yaml elements from webform entity.

--- docroot/modules/custom/webform_serialization/src/Normalizer/WebformNormalizer.php
+++ docroot/modules/custom/webform_serialization/src/Normalizer/WebformNormalizer.php
@@ -5,6 +5,8 @@ namespace Drupal\webform_serialization\Normalizer;
 use Drupal\jsonapi\Normalizer\ConfigEntityNormalizer as JsonapiConfigEntityNormalizer;
 use Drupal\jsonapi\ResourceType\ResourceType;
 use Drupal\webform\Entity\Webform;
+use Drupal\Core\Serialization\Yaml;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 /**
  * Converts a webform into JSON API array structure.
@@ -17,7 +19,21 @@ class WebformNormalizer extends JsonapiConfigEntityNormalizer {
    * {@inheritdoc}
    */
   public function getFields($entity, $bundle, ResourceType $resource_type) {
-    return parent::getFields($entity, $bundle, $resource_type);
+    $enabled_public_fields = parent::getFields($entity, $bundle, $resource_type);
+    if (!empty($enabled_public_fields['elements'])) {
+      try {
+        $parsed = Yaml::decode($enabled_public_fields['elements']);
+        $enabled_public_fields['elements'] = $parsed;
+      }
+      catch (\Exception $exception) {
+        throw new UnprocessableEntityHttpException(
+          sprintf('There was an error parsing the webform configuration. Message: %s.', $exception->getMessage()),
+          $exception
+        );
+      }
+    }
+
+    return $enabled_public_fields;
   }
 
 }
