0a0427b42bf9f2e57e929786577e6ac1b4c7d233
Barrett <barrett.smith@acquia.com>
2017-06-23 14:03:46 -0400

    DOJ-13: Adding custom module (largely stolen from Reservoir UI) to style and provide link to api docs.

---
+++ docroot/modules/custom/foai_ui/src/Controller/OpenApiDocs.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\foia_ui\Controller;
+
+use Drupal\Core\Url;
+use Drupal\foia_ui\FoiaResourceTypeRepository;
+
+/**
+ * Controller for generating documentation pages.
+ */
+class OpenApiDocs {
+
+  /**
+   * Generating documentation for node bundles.
+   *
+   * @param string $entity_type_id
+   *   The entity type.
+   * @param string $node_type
+   *   The entity bundle.
+   *
+   * @return array
+   *   The generated documentation.
+   */
+  public function generateBundleDocs($entity_type_id, $node_type) {
+    $query = [
+      'options[bundle_name]' => $node_type,
+      'options[entity_type_id]' => $entity_type_id,
+    ];
+    return $this->generateDocsFromQuery($query);
+  }
+
+  /**
+   * Generate the default docs.
+   *
+   * @return array
+   *   The generated documentation.
+   */
+  public function generateDocs($entity_mode) {
+    $options = [
+      'entity_mode' => $entity_mode,
+    ];
+    // @todo Remove this once OpenAPI automatically omits docs for entity types that do not have JSON API routes.
+    $options['exclude'] = FoiaResourceTypeRepository::$blacklistedEntityTypeIds;
+    return $this->generateDocsFromQuery(['options' => $options]);
+  }
+
+  /**
+   * Generates the doc for query options.
+   *
+   * @param array $query
+   *   The query options for the OpenAPI generation.
+   *
+   * @return array
+   *   The generated documentation.
+   */
+  protected function generateDocsFromQuery(array $query) {
+    $route_options = [
+      'query' => [
+        '_format' => 'json',
+      ] + $query,
+    ];
+    $build = [
+      '#theme' => 'redoc',
+      '#attributes' => [
+        'no-auto-auth' => TRUE,
+        'scroll-y-offset' => 150,
+      ],
+      '#openapi_url' => Url::fromRoute('openapi.jsonapi', [], $route_options)
+        ->setAbsolute()
+        ->toString(),
+    ];
+    return $build;
+  }
+
+}
