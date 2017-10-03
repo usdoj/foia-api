26f377cf8f6cf23a6b6d188d828c0d89d0b2c8e0
Tom Camp <tom.camp@civicactions.com>
2017-10-03 10:49:35 -0600

    DOJ-120: Cleaing up the queue processing.

--- docroot/modules/custom/foia_webform/src/Plugin/QueueWorker/FoiaSubmissionQueueWorker.php
+++ docroot/modules/custom/foia_webform/src/Plugin/QueueWorker/FoiaSubmissionQueueWorker.php
@@ -2,37 +2,14 @@
 
 namespace Drupal\foia_webform\Plugin\QueueWorker;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Queue\QueueWorkerBase;
-use Drupal\webform\WebformSubmissionInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Annotation\QueueWorker;
 
 /**
- * Provides base functionality for the FoiaForm Queue Workers.
+ * Provides functionality for working with the queued FOIA form submissions.
+ *
+ * @QueueWorker (
+ *   id = "foia_submissions",
+ *   title = @Translation("Manual FOIA Form submitter"),
+ * )
  */
-abstract class FoiaSubmissionQueueWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The form submission.
-   *
-   * @var \Drupal\webform\WebformSubmissionInterface
-   */
-  protected $formSubmission;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(WebformSubmissionInterface $submission) {
-    $this->formSubmission = $submission;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $container->get('entity.manager')->getStorage('webform_submission')
-    );
-  }
-
-}
+class FoiaSubmissionQueueWorker extends QueueWorker {}
