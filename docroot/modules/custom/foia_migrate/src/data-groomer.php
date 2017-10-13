fd136c9bb15294746bb1410b63e1dee4a285f2e7
Tom Camp <tom.camp@civicactions.com>
2017-10-13 10:51:56 -0600

    DOJ-192: Adding year field and processing field migration.

--- docroot/modules/custom/foia_migrate/src/data-groomer.php
+++ docroot/modules/custom/foia_migrate/src/data-groomer.php
@@ -16,6 +16,7 @@ $component_data = extract_components_from_agencies($agency_data['agencies']);
 move_emails_to_email_submission_key($component_data['components']);
 remove_departments_property_from_agencies($agency_data);
 $personnel_data = extract_personnel_from_components($component_data['components']);
+extract_processing_data($component_data['components']);
 
 write_data_to_json_file("{$modified_data_files_directory}/agencies.json", $agency_data);
 write_data_to_json_file("{$modified_data_files_directory}/components.json", $component_data);
@@ -298,5 +299,22 @@ function remove_departments_property_from_agencies(array &$agency_data) {
   $agencies = $agency_data['agencies'];
   foreach ($agencies as $agency) {
     unset($agency->departments);
+  }
+}
+
+/**
+ * Extract that last item in the Request Times Stats data object.
+ *
+ * @param array &$components
+ *   Array of all components.
+ */
+function extract_processing_data(array &$components) {
+  foreach ($components as $key => $component) {
+    if (isset($component->request_time_stats)) {
+      $latest_data = end($component->request_time_stats);
+      $year = key($component->request_time_stats);
+      $components[$key]->latest_request_time_stats = $latest_data;
+      $components[$key]->latest_request_time_stats_year = $year;
+    }
   }
 }
