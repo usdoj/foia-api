d2f4ef787e30479ada1ba810e6bb14ebfd7e2323
Malik Kotob <malikkotob@users.noreply.github.com>
2017-10-27 11:49:02 -0400

    DOJ-122: Update foia request after queue worker processing.

--- docroot/modules/custom/foia_webform/src/Plugin/QueueWorker/FoiaSubmissionQueueWorker.php
+++ docroot/modules/custom/foia_webform/src/Plugin/QueueWorker/FoiaSubmissionQueueWorker.php
@@ -5,9 +5,11 @@ namespace Drupal\foia_webform\Plugin\QueueWorker;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
 use Drupal\foia_request\Entity\FoiaRequest;
+use Drupal\foia_request\Entity\FoiaRequestInterface;
 use Drupal\foia_webform\AgencyLookupServiceInterface;
 use Drupal\foia_webform\FoiaSubmissionServiceFactoryInterface;
 use Drupal\node\Entity\Node;
+use Drupal\webform\Entity\WebformSubmission;
 use Drupal\webform\WebformSubmissionStorageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -73,8 +75,46 @@ class FoiaSubmissionQueueWorker extends QueueWorkerBase implements ContainerFact
     $agencyComponent = Node::load($agencyComponentId);
     $submissionService = $this->foiaSubmissionServiceFactory->get($agencyComponent);
 
+    $foiaRequest->set('field_submission_time', REQUEST_TIME);
     // Submit the form values to the Agency Component.
     $validSubmissionResponse = $submissionService->sendRequestToComponent($foiaRequest, $agencyComponent);
+
+    if ($validSubmissionResponse) {
+      $foiaRequest->setRequestStatus(FoiaRequestInterface::STATUS_SUBMITTED);
+      $submissionMethod = isset($validSubmissionResponse['type']) ? $validSubmissionResponse['type'] : '';
+      $responseCode = isset($validSubmissionResponse['response_code']) ? $validSubmissionResponse['response_code'] : '';
+      $caseManagementId = isset($validSubmissionResponse['id']) ? $validSubmissionResponse['id'] : '';
+      $caseManagementStatusTrackingNumber = isset($validSubmissionResponse['status_tracking_number']) ? $validSubmissionResponse['status_tracking_number'] : '';
+      if ($caseManagementId) {
+        $foiaRequest->set('field_case_management_id', $caseManagementId);
+      }
+      if ($caseManagementStatusTrackingNumber) {
+        $foiaRequest->set('field_tracking_number', $caseManagementStatusTrackingNumber);
+      }
+      $webformSubmissionId = $foiaRequest->get('field_webform_submission_id')->value;
+      $webformSubmission = WebformSubmission::load($webformSubmissionId);
+      $webformSubmission->delete();
+      // @todo change http_code in service api error to response_code
+    }
+    else {
+      $foiaRequest->setRequestStatus(FoiaRequestInterface::STATUS_FAILED);
+      $invalidSubmissionInfo = $submissionService->getSubmissionErrors();
+      $submissionMethod = isset($invalidSubmissionInfo['type']) ? $invalidSubmissionInfo['type'] : '';
+      // @todo change http_code in service api error to response_code
+      $responseCode = isset($invalidSubmissionInfo['response_code']) ? $invalidSubmissionInfo['response_code'] : '';
+      $errorCode = isset($invalidSubmissionInfo['error_code']) ? $invalidSubmissionInfo['error_code'] : '';
+      $errorMessage = isset($invalidSubmissionInfo['message']) ? $invalidSubmissionInfo['message'] : '';
+      $errorDescription = isset($invalidSubmissionInfo['description']) ? $invalidSubmissionInfo['description'] : '';
+      if ($errorCode) {
+        $foiaRequest->set('field_error_code', $errorCode);
+      }
+      // @todo create separate error message and description fields
+      if ($errorMessage || $errorDescription) {
+        $foiaRequest->set('field_error_message', "Message: {$errorMessage}. Description: {$errorDescription}");
+      }
+    }
+    $foiaRequest->setSubmissionMethod($submissionMethod);
+    $foiaRequest->set('field_response_code', $responseCode);
   }
 
 }
