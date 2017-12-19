2d2b0d8dfa97f1876e7f2a942e166ee3cd3e3f17
Tom Camp <tom.camp@civicactions.com>
2017-12-19 14:17:40 -0700

    DOJ-456: Moving FileField lookup to service.

---
+++ docroot/modules/custom/foia_webform/src/FileFieldLookupService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\foia_webform;
+
+use Drupal\webform\WebformInterface;
+
+/**
+ * Class FileFieldLookupService.
+ */
+class FileFieldLookupService {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFileAttachmentElementsOnWebform(WebformInterface $webform) {
+    $elements = $webform->getElementsInitialized();
+    $fileAttachmentElementKeys = [];
+    foreach ($elements as $key => $element) {
+      if (isset($element['#type']) && $element['#type'] == 'managed_file') {
+        $fileAttachmentElementKeys[] = $key;
+      }
+    }
+    return $fileAttachmentElementKeys;
+  }
+
+}
