8dc3764f2444d2c4d10345e84adbccdc0dc87305
Brock Fanning <brockfanning@gmail.com>
2018-01-26 10:25:33 -0500

    Use BLT commands instead of straight Unix stuff

--- box/doj/vm-setup.sh
+++ box/doj/vm-setup.sh
@@ -17,9 +17,10 @@ if [ ! -e "$DRUPAL_BASIC_SETUP_FILE" ]; then
   # Back-stage installation.
   cd /var/www/dojfoia
   composer install
-  cp docroot/sites/default/settings/default.local.settings.php docroot/sites/default/settings/local.settings.php
-  cp -r simplesamlphp/* vendor/simplesamlphp/simplesamlphp/
-  cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32 > salt.txt
+  composer run-script blt-alias
+  source ~/.bash_profile
+  blt setup:settings
+  blt setup:build
 
   # Front-stage installation.
   # Includes a manual Ruby installation because I can't get DrupalVM to set
