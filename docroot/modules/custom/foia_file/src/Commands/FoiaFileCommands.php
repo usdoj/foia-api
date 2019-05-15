d697c5c35643acfd6cb294d1f91c9617ec2506b1
Brock Fanning <brockfanning@gmail.com>
2019-05-15 12:17:09 +0000

    DOJ-884259: Drush commands ported to 9.

---
+++ docroot/modules/custom/foia_file/src/Commands/FoiaFileCommands.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Drupal\foia_file\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ */
+class FoiaFileCommands extends DrushCommands {
+
+  /**
+   * Performs virus scan and updates status on file entities accordingly.
+   *
+   * @param string $path_to_executable
+   *   The absolute path of the virus scanning executable.
+   * @param string $path_to_webform_attachments
+   *   The absolute path of the webform attachments to scan.
+   *
+   * @command virus:scan
+   * @aliases vs,virus-scan
+   */
+  public function scan($path_to_executable, $path_to_webform_attachments) {
+    if (empty($path_to_executable) || empty($path_to_webform_attachments)) {
+      \Drupal::logger('foia_file')->error('Virus scanning executable path or directory to scan missing.');
+      return FALSE;
+    }
+
+    $scan_options = '-r --remove --no-summary';
+    $scan_command = "${path_to_executable} ${scan_options} ${path_to_webform_attachments}";
+    $startTime = microtime(TRUE);
+    $scanOutput = shell_exec($scan_command);
+
+    // Get webform directory on the server.
+    $fileDir = explode('/webform/', $scanOutput, 2);
+    $webformDir = "{$fileDir[0]}/webform/";
+    // Temporarily delete similar paths of the webform dir during processing.
+    $trimmedFiles = str_replace($webformDir, '', $scanOutput);
+
+    // Split string into lines. @todo change to preg split \R?
+    $scans = explode("\n", $trimmedFiles);
+
+    // Set constants.
+    $cleanScanStatuses = ['OK', 'Empty file'];
+    $doNotScanEntityStatuses = ['clean', 'virus'];
+
+    foreach ($scans as $scan) {
+
+      // Validate the scan output status.
+      preg_match('/ (OK|Empty file|FOUND|Removed\.)$/', $scan, $scanStatusWithLeadingSpace);
+      // Do not process scan status "FOUND".
+      // These will be processed under the duplicate "Remove." scan status.
+      if ($scanStatusWithLeadingSpace[0] === ' FOUND') {
+        continue;
+      }
+      if ($scanStatusWithLeadingSpace) {
+        // Trim the leading space.
+        $scanStatus = ltrim($scanStatusWithLeadingSpace[0], " ");
+      }
+      else {
+        \Drupal::logger('foia_file')->warning(
+          "An unexpected output was detected in the virus scan: @scan",
+          ['@scan' => $scan]
+        );
+        continue;
+      }
+
+      $relativeFileName = preg_replace('/: (OK|Empty file|Removed\.)$/', '', $scan);
+      $absoluteFileName = $webformDir . $relativeFileName;
+      $pathParts = pathinfo($absoluteFileName);
+
+      // Log any discrepancies between filesystem and entities.
+      if (in_array($scanStatus, $cleanScanStatuses) && !file_exists($absoluteFileName)) {
+        \Drupal::logger('foia_file')->warning(
+          "The file @absoluteFileName was not detected on the filesystem but was marked as clean by the virus scanner.",
+          ['@absoluteFileName' => $absoluteFileName]
+        );
+      }
+      if ($scanStatus === 'Removed.' && file_exists($absoluteFileName)) {
+        \Drupal::logger('foia_file')->warning(
+          "The file @absoluteFileName was detected on the filesystem and a virus was detected in it. The file should have been deleted by the scanner.",
+          ['@absoluteFileName' => $absoluteFileName]
+        );
+      }
+
+      // Determine file entity id based upon file path & name.
+      $query = \Drupal::entityQuery('file')
+        ->condition('uri', "private://webform/{$relativeFileName}");
+      $fids = $query->execute();
+      if ($fids) {
+        $fid = array_values($fids)[0];
+      }
+
+      // @todo Validate that only one item in fids array.
+
+      // @todo Nice to have >> verify that submission id is same as file path.
+
+      // Update file entity based upon scanStatus.
+      if ($fid) {
+        $file = File::load($fid);
+      }
+      else {
+        $file = NULL;
+      }
+      if ($file && !in_array($file->get('field_virus_scan_status')->getString(), $doNotScanEntityStatuses)) {
+        if (in_array($scanStatus, $cleanScanStatuses)) {
+          if ($file->hasField('field_virus_scan_status')) {
+            $file->set('field_virus_scan_status', 'clean');
+          }
+          else {
+            \Drupal::logger('foia_file')->warning(
+              "The file @absoluteFileName with the ID @fid does not have a \"Virus scan scanStatus\" field and was not able to be marked as clean even though it passed the file scan.",
+              [
+                '@absoluteFileName' => $absoluteFileName,
+                '@fid' => $fid,
+              ]
+            );
+          }
+        }
+        elseif ($scanStatus === "Removed.") {
+          $file->set('field_virus_scan_status', 'virus');
+          \Drupal::logger('foia_file')->warning(
+            "A virus was detected in the file @absoluteFileName. The file has been deleted. The associated Entity ID @fid has been set to virus.",
+            [
+              '@absoluteFileName' => $absoluteFileName,
+              '@fid' => $fid,
+            ]
+          );
+        }
+        $file->save();
+      }
+
+      if (array_search('', $pathParts) === TRUE) {
+        \Drupal::logger('foia_file')->warning(
+          "An invalid path of @path, filename of @relativeFileName, or extension of @extension was detected.",
+          [
+            '@path' => $pathParts['dirname'],
+            '@relativeFileName' => $pathParts['filename'],
+            '@extension' => $pathParts['extension'],
+          ]
+        );
+      }
+    }
+
+    $endTime = microtime(TRUE);
+
+    $executionTime = ($endTime - $startTime);
+
+    \Drupal::logger('foia_file')->info("Virus scanning and file updates completed in {$executionTime} seconds.");
+
+  }
+
+}
---
+++ docroot/modules/custom/foia_request/src/Commands/FoiaRequestCommands.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Drupal\foia_request\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ */
+class FoiaRequestCommands extends DrushCommands {
+
+  /**
+   * Queue clean FOIA request when files have been scanned.
+   *
+   * @command queue:clean-request
+   * @aliases qcr,queue-clean-request
+   */
+  public function cleanRequest() {
+    $requests_to_be_scanned = $this->toBeScanned();
+    if (!empty($requests_to_be_scanned)) {
+      $this->checkRequest($requests_to_be_scanned);
+    }
+  }
+
+  /**
+   * Update in-transit email requests, marking them as submitted if they're old.
+   *
+   * @command assume:emails-arrived
+   * @aliases aea,assume-emails-arrived
+   */
+  public function emailsArrived() {
+    $deliveredEmails = $this->deliveredEmails();
+    foreach ($deliveredEmails as $deliveredEmail) {
+      $foiaRequest = FoiaRequest::load($deliveredEmail->id);
+      $foiaRequest->setRequestStatus(FoiaRequestInterface::STATUS_SUBMITTED);
+      $foiaRequest->save();
+      // We also need to now delete the webform submission.
+      $webformSubmissionId = $foiaRequest->get('field_webform_submission_id')->value;
+      $webformSubmission = WebformSubmission::load($webformSubmissionId);
+      $webformSubmission->delete();
+    }
+  }
+
+  /**
+   * Check each request to see if it can be queued.
+   *
+   * @param array $requests_to_be_scanned
+   *   An array containing FOIA Requests IDs to be checked.
+   */
+  public function checkRequest(array $requests_to_be_scanned) {
+    foreach ($requests_to_be_scanned as $request) {
+      $ready_to_queue = $this->checkFileFields($request);
+      if ($ready_to_queue) {
+        $this->queueRequest($request);
+      }
+    }
+  }
+
+  /**
+   * Checks files for every file field on a given request.
+   *
+   * @param object $request
+   *   An database object containing the submission ID and foia_request ID.
+   *
+   * @return bool
+   *   Returns TRUE if all of the files have been scanned, else FALSE.
+   */
+  public function checkFileFields($request) {
+    $attachment_fields = &drupal_static(__FUNCTION__);
+    $webform_submission = WebformSubmission::load($request->sid);
+
+    if (isset($attachment_fields[$request->webform_id])) {
+      $file_fields = $attachment_fields[$request->webform_id];
+    }
+    else {
+      $webform = $webform_submission->getWebform();
+      $file_field_lookup = \Drupal::service('foia_webform.file_field_lookup_service');
+      $attachment_fields[$request->webform_id] = $file_field_lookup->getFileAttachmentElementsOnWebform($webform);
+      $file_fields = $attachment_fields[$request->webform_id];
+    }
+
+    if (isset($file_fields)) {
+      $data = $webform_submission->getData();
+      foreach ($file_fields as $field_name) {
+        $ready_to_queue = $this->checkFileStatuses($data[$field_name]);
+        if (!$ready_to_queue) {
+          return FALSE;
+        }
+      }
+    }
+    return $ready_to_queue;
+  }
+
+  /**
+   * Query for FOIA requests whose status is set to "To Be Scanned".
+   */
+  public function toBeScanned() {
+    $query = db_select('foia_request', 'fr');
+    $query->addField('fr', 'id');
+    $query->join('foia_request__field_webform_submission_id', 'frid', 'frid.entity_id = fr.id');
+    $query->addField('frid', 'field_webform_submission_id_value', 'sid');
+    $query->join('webform_submission', 'ws', 'ws.sid = frid.field_webform_submission_id_value');
+    $query->addField('ws', 'webform_id');
+    $query->condition('fr.request_status', FoiaRequestInterface::STATUS_SCAN);
+    return $query->execute()->fetchAll();
+  }
+
+  /**
+   * Query for FOIA email requests that are now assumed to have arrived.
+   *
+   * The criteria for this assumption are that they are old enough, based on the
+   * FoiaRequestInterface::ASSUME_DELIVERED_AFTER constant.
+   */
+  public function deliveredEmails() {
+    $query = db_select('foia_request', 'fr');
+    $query->addField('fr', 'id');
+    $query->condition('fr.request_status', FoiaRequestInterface::STATUS_IN_TRANSIT);
+    $query->condition('fr.created', time() - FoiaRequestInterface::ASSUME_DELIVERED_AFTER, '<');
+    return $query->execute()->fetchAll();
+  }
+
+  /**
+   * Check the status of files to see if they've been scanned.
+   *
+   * @param array $file_ids
+   *   An array containing file IDs.
+   */
+  public function checkFileStatuses(array $file_ids) {
+    $ready_to_queue = FALSE;
+    foreach ($file_ids as $file_id) {
+      $file = FileEntity::load($file_id);
+      if ($file->hasField('field_virus_scan_status')) {
+        $file_status = $file->get('field_virus_scan_status')->value;
+        if ($file_status === 'scan') {
+          return FALSE;
+        }
+        else {
+          $ready_to_queue = TRUE;
+        }
+      }
+    }
+    return $ready_to_queue;
+  }
+
+  /**
+   * Adds the clean request to the Queue.
+   *
+   * @param object $request
+   *   An database object containing the submission ID and foia_request ID.
+   */
+  public function queueRequest($request) {
+    $foia_request = FoiaRequest::load($request->id);
+    $enqueueing_status = \Drupal::service('foia_webform.foia_submission_queueing_service')
+      ->enqueue($foia_request);
+
+    if ($enqueueing_status) {
+      $this->updateRequestStatus($foia_request);
+    }
+  }
+
+  /**
+   * Update the Request status after enqueueing.
+   *
+   * @param \Drupal\foia_request\Entity\FoiaRequest $foia_request
+   *   The FOIA Request entity.
+   */
+  public function updateRequestStatus(FoiaRequest $foia_request) {
+    $foia_request->setRequestStatus(FoiaRequestInterface::STATUS_QUEUED);
+    $foia_request->save();
+  }
+
+}
