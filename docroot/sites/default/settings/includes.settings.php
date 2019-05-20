682efe7f399567be904deb402380bab27b1d96e3
Brock Fanning <brockfanning@gmail.com>
2019-05-20 12:21:54 +0000

    DOJ-884259: Move IP check to after the environment-specific settings file.

--- docroot/sites/default/settings/includes.settings.php
+++ docroot/sites/default/settings/includes.settings.php
@@ -5,11 +5,6 @@
  * Site-specific settings.php code for the National FOIA Portal.
  */
 
-if (file_exists(DRUPAL_ROOT . '/sites/acquia.inc')) {
-  require DRUPAL_ROOT . '/sites/acquia.inc';
-  ac_protect_this_site();
-}
-
 $additionalSettingsFiles = [
   '/mnt/gfs/home/' . $ah_group . '/' . $ah_env . '/secrets.settings.php',
 ];
@@ -18,4 +13,9 @@ foreach ($additionalSettingsFiles as $settingsFile) {
   if (file_exists($settingsFile)) {
     require $settingsFile;
   }
+}
+
+if (file_exists(DRUPAL_ROOT . '/sites/acquia.inc')) {
+  require DRUPAL_ROOT . '/sites/acquia.inc';
+  ac_protect_this_site();
 }
