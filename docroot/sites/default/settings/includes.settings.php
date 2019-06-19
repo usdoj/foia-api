3fff76ee5d59af55be8176767d9be7dd4b8908bc
Brock Fanning <brockfanning@gmail.com>
2019-06-19 15:35:59 +0000

    DOJ-53: Settings and Composer changes to support BLT 10.

--- docroot/sites/default/settings/includes.settings.php
+++ docroot/sites/default/settings/includes.settings.php
@@ -2,20 +2,31 @@
 
 /**
  * @file
+ * Includes settings files on Acquia.
+ */
+
+use Acquia\Blt\Robo\Common\EnvironmentDetector;
+
+/**
+ * @file
  * Site-specific settings.php code for the National FOIA Portal.
  */
 
-$additionalSettingsFiles = [
-  '/mnt/gfs/home/' . $ah_group . '/' . $ah_env . '/secrets.settings.php',
-];
+if (EnvironmentDetector::isAhEnv()) {
+  $ah_group = EnvironmentDetector::getAhGroup();
+  $ah_env = EnvironmentDetector::getAhEnv();
+  $additionalSettingsFiles = [
+    '/mnt/gfs/home/' . $ah_group . '/' . $ah_env . '/secrets.settings.php',
+  ];
 
-foreach ($additionalSettingsFiles as $settingsFile) {
-  if (file_exists($settingsFile)) {
-    require $settingsFile;
+  foreach ($additionalSettingsFiles as $settingsFile) {
+    if (file_exists($settingsFile)) {
+      require $settingsFile;
+    }
   }
-}
 
-if (file_exists(DRUPAL_ROOT . '/sites/acquia.inc')) {
-  require DRUPAL_ROOT . '/sites/acquia.inc';
-  ac_protect_this_site();
+  if (file_exists(DRUPAL_ROOT . '/sites/acquia.inc')) {
+    require DRUPAL_ROOT . '/sites/acquia.inc';
+    ac_protect_this_site();
+  }
 }
