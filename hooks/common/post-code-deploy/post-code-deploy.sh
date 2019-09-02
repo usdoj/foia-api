ff4d2ceafaaa5cd77bd6db833a59844629b660ff
Barrett Smith <barrett.smith@acquia.com>
2019-09-02 15:04:53 -0400

    FOIA-000: Adding post-code-deploy hook to copy the phpdocx library to files directory and symlink for docroot/libraries.

---
+++ hooks/common/post-code-deploy/post-code-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Cloud Hook: post-code-deploy
+#
+# The post-code-deploy hook is run whenever you use the Workflow page to
+# deploy new code to an environment, either via drag-drop or by selecting
+# an existing branch or tag from the Code drop-down list. See
+# ../README.md for details.
+#
+# Usage: post-code-deploy site target-env source-branch deployed-tag repo-url
+#                         repo-type
+
+set -ev
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+# Prep for BLT commands.
+repo_root="/var/www/html/$site.$target_env"
+export PATH=$repo_root/vendor/bin:$PATH
+cd $repo_root
+
+blt artifact:ac-hooks:post-code-deploy $site $target_env $source_branch $deployed_tag $repo_url $repo_type --environment=$target_env -v --no-interaction -D drush.ansi=false
+
+cp -r ~/phpdocx "$repo_root/docroot/sites/default/files/"
+
+set +v
