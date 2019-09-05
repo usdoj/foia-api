c18a631d1c77b69f9a5f3a1268623f2e7b2c8746
Aaron Manire <amanire@isovera.com>
2019-09-05 13:50:47 -0400

    FOIA-30: Add lessThanequalsSum method and example to report validation.

--- docroot/modules/custom/foia_ui/js/foia_ui.validation.js
+++ docroot/modules/custom/foia_ui/js/foia_ui.validation.js
@@ -6,6 +6,14 @@
         success: "valid"
       });
 
+      jQuery.validator.addMethod("lessThanEqualSum", function(value, element, params) {
+        var sum = 0;
+        params.forEach(function(param) {
+          sum += Number($(param).val());
+        });
+        return this.optional(element) || value <= sum;
+      }, "Must equal less than equal a sum of other fields.");
+
       $('#node-annual-foia-report-data-form').validate({
 
         invalidHandler: function(event, validator) {
@@ -40,9 +48,23 @@
             equalTo: "#edit-field-overall-vb1-total-0-value"
           },
           // V.B.(1) Agency Overall Number of Full Denials Based on Exemptions
-          "field_overall_xiie1_received_cur[0][value]": {
-            min: 2,
-            max: 4
+          "field_foia_requests_vb1[0][subform][field_full_denials_ex][0][value]": {
+            lessThanEqualSum: [
+              "#edit-field-overall-vb3-ex-1-0-value",
+              "#edit-field-overall-vb3-ex-2-0-value",
+              "#edit-field-overall-vb3-ex-3-0-value",
+              "#edit-field-overall-vb3-ex-4-0-value",
+              "#edit-field-overall-vb3-ex-5-0-value",
+              "#edit-field-overall-vb3-ex-6-0-value",
+              "#edit-field-overall-vb3-ex-7-a-0-value",
+              "#edit-field-overall-vb3-ex-7-b-0-value",
+              "#edit-field-overall-vb3-ex-7-c-0-value",
+              "#edit-field-overall-vb3-ex-7-d-0-value",
+              "#edit-field-overall-vb3-ex-7-e-0-value",
+              "#edit-field-overall-vb3-ex-7-f-0-value",
+              "#edit-field-overall-vb3-ex-8-0-value",
+              "#edit-field-overall-vb3-ex-9-0-value"
+            ]
           }
         },
 
@@ -55,6 +77,9 @@
           "field_overall_req_processed_yr[0][value]" : {
             equalTo: "Must match V.B.(1) Agency Overall Total"
           },
+          "field_foia_requests_vb1[0][subform][field_full_denials_ex][0][value]": {
+            lessThanEqualSum: "This field should be no more than the sum of the fields overall_vb3_ex_1 through overall_vb3_ex_9."
+          }
         }
       });
       $('input#edit-submit').prop('disabled', true);
