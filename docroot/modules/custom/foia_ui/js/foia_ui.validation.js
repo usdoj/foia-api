3fa9289e7fb852090287064826ccce118624678d
Aaron Manire <amanire@isovera.com>
2019-09-20 08:50:30 -0400

    FOIA-90: XII.D.(2) validation (#160)
    * FOIA-90: Add XII.D.(2). Overall Backlogged Requests Current validation.
    
    * FOIA-90: Add XII.D.(2). Number of Backlogged Requests validation.

--- docroot/modules/custom/foia_ui/js/foia_ui.validation.js
+++ docroot/modules/custom/foia_ui/js/foia_ui.validation.js
@@ -1131,6 +1131,24 @@
         }
       });
 
+      // XII.D.(2). Number of Backlogged Requests as of End of the Fiscal Year from Current Annual Report
+      $( "input[name*='field_foia_xiid2']").filter("input[name*='field_back_cur_yr']").each(function() {
+        $(this).rules( "add", {
+          equalToComp: $( "input[name*='field_foia_xiia']").filter("input[name*='field_back_app_end_yr']"),
+          messages: {
+            equalToComp: "Must match XII.A. Number of Backlogged Requests as of End of Fiscal Year",
+
+          }
+        });
+      });
+
+      // XII.D.(2). Agency Overall Number of Backlogged Requests as of End of the Fiscal Year from Current Annual Report
+      $( "#edit-field-overall-xiid2-back-cur-yr-0-value").rules( "add", {
+        equalTo: "#edit-field-overall-xiia-back-req-end-0-value",
+        messages: {
+          equalTo: "Must match XII.A. Agency Overall Number of Backlogged Requests as of End of Fiscal Year",
+        }
+      });
     }
   };
 
