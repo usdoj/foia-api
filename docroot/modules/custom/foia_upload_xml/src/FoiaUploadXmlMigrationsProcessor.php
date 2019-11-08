99afa3e0d9c4628f4a6065e039b2e1fc2e14d27c
Jesse Day <jesseday@users.noreply.github.com>
2019-11-08 09:24:59 -0700

    FOIA-172: Queue report uploads. (#277)
    * FOIA-172: Add a report import queue worker.
    
    * FOIA-172: Use new processor in batch importer.
    
    * FOIA-172: Fixing doc block param class references.
    
    * FOIA-172: Add queueing to the report upload form.
    
    * FOIA-172: Add class to get data from report file.
    
    * FOIA-172: Update the report upload validation.
    
    * FOIA-172: Update how report uploads are renamed.
    
    * FOIA-172: Add the queue_ui module.
    
    * FOIA-172: Remove unused class property.
    
    * FOIA-172: Fix migrate upload source before import.
    
    * FOIA-172: Set the custom class for all migrations.
    
    * FOIA-172: Enable queue_ui module.
    
    * FOIA-172: Redirect to after queueing report.
    
    * FOIA-172: Default queue item agency and year.
    
    * FOIA-172: Fix override of source in report import.
    
    * FOIA-172: Update queue messaging.

---
+++ docroot/modules/custom/foia_upload_xml/src/FoiaUploadXmlMigrationsProcessor.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Drupal\foia_upload_xml;
+
+use Drupal\file\FileInterface;
+use Drupal\migrate\MigrateMessage;
+use Drupal\Core\Cache\NullBackend;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Component\Utility\NestedArray;
+use Drupal\migrate_tools\MigrateExecutable;
+use Drupal\migrate\MigrateMessageInterface;
+use Drupal\migrate\Plugin\MigrationPluginManager;
+
+/**
+ * Configures and runs migrations for the batch processor or import worker.
+ *
+ * @package Drupal\foia_upload_xml
+ */
+class FoiaUploadXmlMigrationsProcessor {
+
+  /**
+   * The migration plugin manager.
+   *
+   * @var \Drupal\migrate\Plugin\MigrationPluginManager
+   */
+  protected $migrationPluginManager;
+
+  /**
+   * The migration message.
+   *
+   * @var \Drupal\migrate\MigrateMessageInterface
+   */
+  protected $migrateMessage;
+
+  /**
+   * Configuration overrides if processing all migrations.
+   *
+   * @var array
+   */
+  protected $sourceOverrides;
+
+  /**
+   * FoiaUploadXmlMigrationsProcessor constructor.
+   *
+   * @param \Drupal\migrate\Plugin\MigrationPluginManager $migrationPluginManager
+   *   The migration plugin manager.
+   */
+  public function __construct(MigrationPluginManager $migrationPluginManager) {
+    $this->migrationPluginManager = $migrationPluginManager;
+    $this->migrationPluginManager->setCacheBackend(new NullBackend('discovery'), 'migration_plugins', ['migration_plugins']);
+    $this->migrateMessage = new MigrateMessage();
+    $this->sourceOverrides = [];
+  }
+
+  /**
+   * Process a single migration.
+   *
+   * @param string $migration_id
+   *   The migration id to be processed.
+   * @param array $configuration
+   *   Configuration to pass to the migrationPluginManager when creating the
+   *   migration instance.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   * @throws \Drupal\migrate\MigrateException
+   */
+  public function process($migration_id, array $configuration = []) {
+    $configuration = NestedArray::mergeDeep($configuration, $this->sourceOverrides);
+    /** @var \Drupal\foia_upload_xml\Plugin\migrate\FoiaUploadXmlMigration $migration */
+    $migration = $this->migrationPluginManager->createInstance($migration_id, $configuration);
+    // Ensure that the migration's source url is only using the url as
+    // configured by the FoiaUploadXmlMigrationsProcessor::setSourceFile()
+    // method, if it has been set.  This overwrites the source url that
+    // is set based on the migration's configuration and that may or may not
+    // exist.
+    $migration = $migration->setMigrationSourceUrls($migration);
+    $migration->getIdMap()->prepareUpdate();
+    $executable = new MigrateExecutable($migration, $this->migrateMessage);
+    $executable->import();
+  }
+
+  /**
+   * Process all the migrations at once.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   * @throws \Drupal\migrate\MigrateException
+   */
+  public function processAll() {
+    foreach ($this->getMigrationsList() as $migration_id) {
+      $this->process($migration_id, $this->sourceOverrides);
+    }
+  }
+
+  /**
+   * Load the migrations, set them to use the source file's path, and save them.
+   *
+   * @param \Drupal\file\FileInterface $file
+   *   The current migration's source file.
+   *
+   * @return $this
+   *   The current object, for chaining.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function setSourceFile(FileInterface $file) {
+    $this->sourceOverrides['source']['urls'] = $file->getFileUri();
+
+    return $this;
+  }
+
+  /**
+   * Set the user id to be passed to all migrations.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   The user id to pass to the migration configuration when processing.
+   *
+   * @return $this
+   */
+  public function setUser(AccountInterface $user) {
+    $this->sourceOverrides['source']['constants']['user_id'] = $user->id();
+
+    return $this;
+  }
+
+  /**
+   * Change the MigrateMessage class if need be.
+   *
+   * @param \Drupal\migrate\MigrateMessageInterface $messenger
+   *   The new migrate message object.
+   *
+   * @return $this
+   */
+  public function setMessenger(MigrateMessageInterface $messenger) {
+    $this->migrateMessage = $messenger;
+
+    return $this;
+  }
+
+  /**
+   * Fetches an array of migrations to run to import the Annual Report XML.
+   *
+   * @return string[]
+   *   List of migrations.
+   */
+  public function getMigrationsList() {
+    $migrations_list = [
+      'component',
+      'component_ix_personnel',
+      'component_iv_statutes',
+      'component_va_requests',
+      'component_vb1_requests',
+      'component_vb2_requests',
+      'component_vb3_requests',
+      'component_via_disposition',
+      'component_vib_disposition',
+      'component_vic1_applied_exemptions',
+      'component_vic2_nonexemption_denial',
+      'component_vic3_other_denial',
+      'component_vic4_response_time',
+      'component_vic5_oldest_pending',
+      'component_viia_processed_requests',
+      'component_viib_processed_requests',
+      'component_viic1_simple_response',
+      'component_viic2_complex_response',
+      'component_viic3_expedited_response',
+      'component_viid_pending_requests',
+      'component_viie_oldest_pending',
+      'component_viiia_expedited_processing',
+      'component_viiib_fee_waiver',
+      'component_xia_subsection_c',
+      'component_xib_subsection_a2',
+      'component_xiia',
+      'component_xiib',
+      'component_xiic',
+      'component_xiid1',
+      'component_xiid2',
+      'component_xiie1',
+      'component_xiie2',
+      'component_x_fees',
+      'foia_vb2_other',
+      'foia_vic3_other',
+      'foia_iv_details',
+      'foia_iv_statute',
+      'foia_va_requests',
+      'foia_vb1_requests',
+      'foia_vb2',
+      'foia_vb3_requests',
+      'foia_via_disposition',
+      'foia_vib_disposition',
+      'foia_vic1_applied_exemptions',
+      'foia_vic2_nonexemption_denial',
+      'foia_vic3',
+      'foia_vic4_response_time',
+      'foia_vic5_oldest_pending',
+      'foia_viia_processed_requests',
+      'foia_viib_processed_requests',
+      'foia_viic1_simple_response',
+      'foia_viic2_complex_response',
+      'foia_viic3_expedited_response',
+      'foia_viid_pending_requests',
+      'foia_viie_oldest_pending',
+      'foia_viiia_expedited_processing',
+      'foia_viiib_fee_waiver',
+      'foia_ix_personnel',
+      'foia_x_fees',
+      'foia_xia_subsection_c',
+      'foia_xib_subsection_a2',
+      'foia_xiia',
+      'foia_xiib',
+      'foia_xiic',
+      'foia_xiid1',
+      'foia_xiid2',
+      'foia_xiie1',
+      'foia_xiie2',
+      'foia_agency_report',
+    ];
+
+    return $migrations_list;
+  }
+
+}
---
+++ docroot/modules/custom/foia_upload_xml/src/Plugin/migrate/FoiaUploadXmlMigration.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\foia_upload_xml\Plugin\migrate;
+
+use Drupal\migrate\Plugin\Migration;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\Plugin\RequirementsInterface;
+use Drupal\migrate\Exception\RequirementsException;
+
+/**
+ * Custom migration plugin for report xml upload processing.
+ *
+ * @package Drupal\foia_upload_xml\Plugin\migrate
+ */
+class FoiaUploadXmlMigration extends Migration {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkRequirements() {
+    // Check whether the current migration source and destination plugin
+    // requirements are met or not.
+    if ($this->getSourcePlugin() instanceof RequirementsInterface) {
+      $this->getSourcePlugin()->checkRequirements();
+    }
+    if ($this->getDestinationPlugin() instanceof RequirementsInterface) {
+      $this->getDestinationPlugin()->checkRequirements();
+    }
+
+    if (empty($this->requirements)) {
+      // There are no requirements to check.
+      return;
+    }
+    /** @var \Drupal\migrate\Plugin\MigrationInterface[] $required_migrations */
+    $required_migrations = $this->getMigrationPluginManager()->createInstances($this->requirements);
+
+    $missing_migrations = array_diff($this->requirements, array_keys($required_migrations));
+    // Check if the dependencies are in good shape.
+    foreach ($required_migrations as $migration_id => $required_migration) {
+      // Configures the source url of dependent migrations to use the same
+      // source url as the current migration.  This ensures that when checking
+      // that dependencies have run, the dependencies attempt to access data
+      // in the correct, and existing, source file.
+      $required_migration = $this->setMigrationSourceUrls($required_migration);
+      if (!$required_migration->allRowsProcessed()) {
+        $missing_migrations[] = $migration_id;
+      }
+    }
+    if ($missing_migrations) {
+      throw new RequirementsException('Missing migrations ' . implode(', ', $missing_migrations) . '.', ['requirements' => $missing_migrations]);
+    }
+  }
+
+  /**
+   * Set source urls of a migration to match the current migration's source url.
+   *
+   * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+   *   The migration to set the source urls of.
+   *
+   * @return \Drupal\migrate\Plugin\MigrationInterface
+   *   The migration with updated source urls.
+   */
+  public function setMigrationSourceUrls(MigrationInterface $migration) {
+    $source = $migration->getSourceConfiguration();
+    $source['urls'] = $this->configuration['source']['urls'] ?? $source['urls'];
+    $migration->set('source', $source);
+
+    return $migration;
+  }
+
+}
