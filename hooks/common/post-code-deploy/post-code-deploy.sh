b306d249a4d8de55ae400627112bae00c8b2d965
Brock Fanning <brockfanning@gmail.com>
2019-11-14 09:01:26 -0500

    Clear cache before config import

--- hooks/common/post-code-deploy/post-code-deploy.sh
+++ hooks/common/post-code-deploy/post-code-deploy.sh
@@ -24,6 +24,8 @@ repo_root="/var/www/html/$site.$target_env"
 export PATH=$repo_root/vendor/bin:$PATH
 cd $repo_root
 
+drush @$site.$target_env cr
+
 blt artifact:ac-hooks:post-code-deploy $site $target_env $source_branch $deployed_tag $repo_url $repo_type --environment=$target_env -v --no-interaction -D drush.ansi=false
 
 set +v
