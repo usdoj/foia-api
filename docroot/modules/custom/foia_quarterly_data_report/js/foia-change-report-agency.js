5afb11c1ef7d2eb3096ef1f41b0b1c89619d83b4
Brock Fanning <brockfanning@gmail.com>
2021-04-28 12:09:26 -0400

    FOIA-0: Working populate button.

--- docroot/modules/custom/foia_quarterly_data_report/js/foia-change-report-agency.js
+++ docroot/modules/custom/foia_quarterly_data_report/js/foia-change-report-agency.js
@@ -31,22 +31,51 @@
     },
 
     addPopulateComponentsButton: function() {
-      $('#edit-field-quarterly-component-data-wrapper').once('foia-add-populate-button').each(function() {
+      var fieldWrapperSelector = '#edit-field-quarterly-component-data-wrapper',
+          addMoreSelector = 'input[name="field_quarterly_component_data_quarterly_component_data_add_more"]',
+          existingComponentSelector = '#edit-field-quarterly-component-data-wrapper tbody tr',
+          checkedComponentSelector = '#edit-field-agency-components input:checked',
+          componentDropdownSelector = '#edit-field-quarterly-component-data-wrapper table tr:last-child .field--name-field-agency-component select';
+      $(fieldWrapperSelector).once('foia-add-populate-button').each(function() {
         var $button = $('<button class="button">Add placeholders for component data below</button>');
         $(this).prepend($button);
         $button.click(function(evt) {
-          var $rows = $('#field-quarterly-component-data-values tbody tr');
-          var $components = $('#edit-field-agency-components input:checked');
-          if ($rows.length > 0) {
-            alert('Placeholders cannot be added while there are existing entries. Please remove all entries and try again.');
-          }
-          else {
-            console.log($components.length);
-          }
           evt.preventDefault();
+          if ($(existingComponentSelector).length > 0) {
+            alert('Placeholders cannot be added while there are existing entries. Please remove all entries and try again.');
+            return;
+          }
+          var $components = $(checkedComponentSelector),
+              numComponents = $components.length;
+          if (numComponents === 0) {
+            alert('First select the components you want using the checkboxes above.');
+            return;
+          }
+          var currentComponent = 0;
+          function clickAddMoreButton() {
+            $(addMoreSelector).trigger('mousedown');
+          }
+          function populateNextComponent() {
+            var componentNodeId = $components.eq(currentComponent).val();
+            $(componentDropdownSelector).val(componentNodeId);
+
+            currentComponent += 1;
+            if (currentComponent < numComponents) {
+              clickAddMoreButton();
+            }
+            else {
+              alert('Finished adding placeholders.');
+            }
+          }
+          $(document).on('ajaxStop', function() {
+            if (currentComponent < numComponents) {
+              populateNextComponent(currentComponent);
+            }
+          });
+          // Kick things off with the first click.
+          clickAddMoreButton();
         });
       });
-
     }
   }
 })(jQuery, drupalSettings);
