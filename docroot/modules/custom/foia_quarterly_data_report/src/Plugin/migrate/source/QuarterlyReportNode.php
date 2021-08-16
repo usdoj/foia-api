9432cc422b0d859046ee25531f9c742dbccb9ef1
Brock Fanning <brockfanning@gmail.com>
2021-08-16 13:54:28 -0400

    FOIA-0: Migration of quarterly reports for 2020.

---
+++ docroot/modules/custom/foia_quarterly_data_report/src/Plugin/migrate/source/QuarterlyReportNode.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace Drupal\foia_quarterly_data_report\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Hello world.
+ *
+ * @MigrateSource(
+ *   id = "quarterly_report_node",
+ *   source_module = "foia_quarterly_data_report",
+ * )
+ */
+class QuarterlyReportNode extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this
+      ->select('quarterlyoldest', 'qo')
+      ->fields('qo', [
+        'id',
+        'CompId',
+        'Year',
+        'Quarter',
+        'Oldest',
+        'Closed',
+      ])
+      ->fields('ag', [
+        'Component',
+        'USagency',
+        'Level',
+        'Description',
+      ]);
+
+    // For now we are only migrating 2020.
+    $query->condition('Year', '2020');
+
+    $query->condition('Level', 1);
+    $query->join('agencies', 'ag', 'ag.id = qo.CompId');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'id' => $this->t('id'),
+      'CompId' => $this->t('CompId'),
+      'Year' => $this->t('Year'),
+      'Quarter' => $this->t('Quarter'),
+      'Oldest' => $this->t('Oldest'),
+      'Closed' => $this->t('Closed'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+        'alias' => 'qo',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $year = $row->getSourceProperty('Year');
+    $quarter = $row->getSourceProperty('Quarter');
+
+    $agency_abbreviation = $this->fixAgencyAbbreviation($row->getSourceProperty('USagency'));
+    $agency_id = $this->getAgencyFromAbbreviation($agency_abbreviation);
+    if (!$agency_id) {
+      print('Unknown agency abbreviation: ' . $agency_abbreviation . PHP_EOL);
+      return FALSE;
+    }
+
+    // Get the overall data.
+    $legacy_id = $row->getSourceProperty('CompId');
+    $query = $this->select('quarterlyrequest', 'qr');
+    $query->fields('qr', ['Received', 'Processed', 'Backlog']);
+    $query->condition('Year', $year);
+    $query->condition('Quarter', $quarter);
+    $query->condition('CompId', $legacy_id);
+    $overall = $query->execute()->fetchAssoc();
+
+    // Get all of the component data.
+    $database = \Drupal::database();
+    $paragraphs = [];
+    $query = $this->select('quarterlyrequest', 'qr');
+    $query->fields('qr', ['id', 'Year', 'Quarter', 'CompId']);
+    $query->fields('ag', ['Level', 'USagency']);
+    $query->join('agencies', 'ag', 'ag.id = qr.CompId');
+    $query->condition('Year', $year);
+    $query->condition('Quarter', $quarter);
+    $query->condition('USagency', $row->getSourceProperty('USagency'));
+    $result = $query->execute();
+    foreach ($result as $record) {
+      $legacy_id = $record['id'];
+      $id_query = $database->select('migrate_map_quarterly_report_paragraph', 'mm');
+      $id_query->condition('sourceid1', $legacy_id);
+      $id_query->fields('mm', ['destid1', 'destid2']);
+      $paragraph_ids = $id_query->execute()->fetchAssoc();
+      if (!empty($paragraph_ids)) {
+        $paragraphs[] = [
+          'target_id' => $paragraph_ids['destid1'],
+          'target_revision_id' => $paragraph_ids['destid2'],
+        ];
+      }
+    }
+
+    // Get all of the component node IDs.
+    $component_ids = [];
+    foreach ($paragraphs as $paragraph) {
+      $entity = $entity = \Drupal::entityTypeManager()->getStorage('paragraph')->load($paragraph['target_id']);
+      $component_id = $entity->get('field_agency_component')->target_id;
+      $component_ids[$component_id] = TRUE;
+    }
+    $component_ids = array_keys($component_ids);
+
+    $row->setSourceProperty('backlogged_oa', $overall['Backlog']);
+    $row->setSourceProperty('processed_oa', $overall['Processed']);
+    $row->setSourceProperty('received_oa', $overall['Received']);
+
+    $row->setSourceProperty('component_data', $paragraphs);
+    $row->setSourceProperty('components', $component_ids);
+
+    $row->setSourceProperty('year', intval($year));
+    $row->setSourceProperty('quarter', intval($quarter));
+    $row->setSourceProperty('pending', $row->getSourceProperty('Oldest'));
+    $row->setSourceProperty('closed', $row->getSourceProperty('Closed'));
+    $row->setSourceProperty('agency', $agency_id);
+
+    // Start published.
+    $row->setSourceProperty('status', 1);
+    $row->setSourceProperty('moderation_state', 'published');
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * Get the agency tid from the abbreviation.
+   *
+   * @param string $abbreviation
+   *   The abbreviation of the agency.
+   *
+   * @return int
+   *   The taxonomy term id of the agency.
+   */
+  private function getAgencyFromAbbreviation($abbreviation) {
+    $agencies = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'vid' => 'agency',
+        'field_agency_abbreviation' => $abbreviation,
+      ]);
+    if ($agency = reset($agencies)) {
+      return $agency->id();
+    }
+    return FALSE;
+  }
+
+  /**
+   * Fix an agency abbreviation.
+   *
+   * @param string $abbreviation
+   *   The agency abbreviation.
+   *
+   * @return string
+   *   The fixed abbreviation.
+   */
+  private function fixAgencyAbbreviation($abbreviation) {
+    $fixes = [
+      'Ex-Im Bank' => 'EXIM Bank',
+      'U.S. DOL' => 'DOL',
+      'US RRB' => 'USRRB',
+      'U.S. CPSC' => 'CPSC',
+      'State' => 'DOS',
+    ];
+    if (!empty($fixes[$abbreviation])) {
+      return $fixes[$abbreviation];
+    }
+    return $abbreviation;
+  }
+
+}
--- docroot/modules/custom/foia_quarterly_data_report/src/Plugin/migrate/source/QuarterlyReportParagraph.php
+++ docroot/modules/custom/foia_quarterly_data_report/src/Plugin/migrate/source/QuarterlyReportParagraph.php
@@ -27,8 +27,19 @@ class QuarterlyReportParagraph extends SqlBase {
         'Quarter',
         'Received',
         'Processed',
-        'Backlogged',
+        'Backlog',
+      ])
+      ->fields('ag', [
+        'Component',
+        'USagency',
+        'Level',
+        'Description',
       ]);
+
+    // For now we are only migrating 2020.
+    $query->condition('Year', '2020');
+
+    $query->join('agencies', 'ag', 'ag.id = qr.CompId');
     return $query;
   }
 
@@ -42,7 +53,7 @@ class QuarterlyReportParagraph extends SqlBase {
       'Quarter' => $this->t('Quarter'),
       'Received' => $this->t('Received'),
       'Processed' => $this->t('Processed'),
-      'Backlogged' => $this->t('Backlogged'),
+      'Backlog' => $this->t('Backlog'),
     ];
     return $fields;
   }
@@ -63,9 +74,172 @@ class QuarterlyReportParagraph extends SqlBase {
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
-    print_r($row);
+    $year = $row->getSourceProperty('Year');
+
+    // For now we will only migrate 2020 reports.
+    if ($year != '2020') {
+      return FALSE;
+    }
+
+    $level = $row->getSourceProperty('Level');
+
+    $agency_abbreviation = $this->fixAgencyAbbreviation($row->getSourceProperty('USagency'));
+    $component_abbreviation = $this->fixComponentAbbreviation($agency_abbreviation, $row->getSourceProperty('Component'));
+    $agency_id = $this->getAgencyFromAbbreviation($agency_abbreviation);
+    if (!$agency_id) {
+      print('Unknown agency abbreviation: ' . $agency_abbreviation . PHP_EOL);
+      return FALSE;
+    }
+
+    $component_id = FALSE;
+    if ($level == '1') {
+      // For level-1 we need to figure out if this is a centralized agency.
+      $centralized_component_id = $this->getCentralizedAgencyComponent($agency_id);
+      if ($centralized_component_id) {
+        $component_id = $centralized_component_id;
+      }
+      else {
+        // Skip decentralized agency overall numbers.
+        // We'll get those later in QuarterlyReportNode.
+        return FALSE;
+      }
+    }
+
+    if (!$component_id) {
+      $component_id = $this->getAgencyComponentFromAbbreviation($component_abbreviation, $agency_id);
+    }
+
+    if (!$component_id) {
+      print('Unknown component abbreviation: ' . $component_abbreviation . PHP_EOL);
+      print('  (in agency: ' . $agency_abbreviation . ')' . PHP_EOL);
+      return FALSE;
+    }
+
+    $row->setSourceProperty('backlogged', $row->getSourceProperty('Backlog'));
+    $row->setSourceProperty('received', $row->getSourceProperty('Received'));
+    $row->setSourceProperty('processed', $row->getSourceProperty('Processed'));
+    $row->setSourceProperty('agency_component', $component_id);
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * Get the agency tid from the abbreviation.
+   *
+   * @param string $abbreviation
+   *   The agency abbreviation.
+   *
+   * @return int
+   *   The taxonomy term id of the agency.
+   */
+  private function getAgencyFromAbbreviation($abbreviation) {
+    $agencies = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'vid' => 'agency',
+        'field_agency_abbreviation' => $abbreviation,
+      ]);
+    if ($agency = reset($agencies)) {
+      return $agency->id();
+    }
     return FALSE;
-    // Temporarily replacing: return parent::prepareRow($row);
+  }
+
+  /**
+   * Get the agency component nid from the abbreviation.
+   *
+   * @param string $abbreviation
+   *   The agency component abbreviation.
+   * @param int $agency_id
+   *   The Drupal id of the agency that the component is in.
+   *
+   * @return int
+   *   The node id of the agency component.
+   */
+  private function getAgencyComponentFromAbbreviation($abbreviation, $agency_id) {
+    $components = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadByProperties([
+        'type' => 'agency_component',
+        'field_agency' => $agency_id,
+        'field_agency_comp_abbreviation' => $abbreviation,
+      ]);
+    if ($component = reset($components)) {
+      return $component->id();
+    }
+    return FALSE;
+  }
+
+  /**
+   * Fix an agency abbreviation.
+   *
+   * @param string $abbreviation
+   *   The agency abbreviation.
+   *
+   * @return string
+   *   The fixed abbreviation.
+   */
+  private function fixAgencyAbbreviation($abbreviation) {
+    $fixes = [
+      'Ex-Im Bank' => 'EXIM Bank',
+      'U.S. DOL' => 'DOL',
+      'US RRB' => 'USRRB',
+      'U.S. CPSC' => 'CPSC',
+      'State' => 'DOS',
+    ];
+    if (!empty($fixes[$abbreviation])) {
+      return $fixes[$abbreviation];
+    }
+    return $abbreviation;
+  }
+
+  /**
+   * Fix a component abbreviation.
+   *
+   * @param string $agency_abbreviation
+   *   The agency abbreviation.
+   * @param string $component_abbreviation
+   *   The component abbreviation.
+   *
+   * @return string
+   *   The fixed abbreviation.
+   */
+  private function fixComponentAbbreviation($agency_abbreviation, $component_abbreviation) {
+    $fixes = [
+      'DOT' => [
+        'SLSDC' => 'GLS',
+      ],
+    ];
+    if (!empty($fixes[$agency_abbreviation][$component_abbreviation])) {
+      return $fixes[$agency_abbreviation][$component_abbreviation];
+    }
+    return $component_abbreviation;
+  }
+
+  /**
+   * Get the centralized component id of an agency, if it exists.
+   *
+   * @param int $agency_id
+   *   The Drupal id of the agency that the component is in.
+   *
+   * @return int
+   *   The node id of the agency component.
+   */
+  private function getCentralizedAgencyComponent($agency_id) {
+    $components = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadByProperties([
+        'type' => 'agency_component',
+        'field_agency' => $agency_id,
+        'status' => 1,
+      ]);
+    foreach ($components as $component) {
+      $centralized = $component->field_is_centralized->value;
+      if ($centralized) {
+        return $component->id();
+      }
+    }
+    return FALSE;
   }
 
 }
