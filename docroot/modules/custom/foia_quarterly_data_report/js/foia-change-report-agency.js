e94ecc34e884a6249abdcd7364895ce059761ae6
brockfanning <brockfanning@gmail.com>
2021-09-15 14:09:19 -0400

    Quarterly reports (#320)
    * FOIA-0: Initial quarterly content type.
    
    * FOIA-0: Custom module for quarterly reports.
    
    * FOIA-0: More config for quarterly reports.
    
    * Temp progress
    
    * FOIA-0: Working populate button.
    
    * FOIA-0: Default values for quarterly report fields.
    
    * FOIA-0: Permissions for quarterly reports.
    
    * FOIA-0: Rename quarterly fields.
    
    * FOIA-0: JSONAPI configuration.
    
    * FOIA-0: Permissions/workflows for quarterly.
    
    * FOIA-0: Updates.
    
    * FOIA-0: Moderation page for quarterly reports.
    
    * FOIA-0: Access-related tweaks.
    
    * FOIA-0: Trying templates for quarterly Word docs.
    
    * FOIA-0: More docx tweaks.
    
    * FOIA-0: Bugfix.
    
    * FOIA-0: Template variables for quarterly reports.
    
    * FOIA-0: Value only.
    
    * FOIA-0: Value only.
    
    * FOIA-0: Value only.
    
    * FOIA-0: Add a send-to-agency transition.
    
    * FOIA-0: Show moderation state in quarterly edit form.
    
    * FOIA-0: Add a published-to-submitted transition.
    
    * FOIA-0: Tweaks to Word header.
    
    * FOIA-0: Try again
    
    * FOIA-0: Try again
    
    * FOIA-0: Controller for quarterly year route.
    
    * FOIA-0: Controller for quarterly year route.
    
    * FOIA-0: Vertical tabs for node page.
    
    * FOIA-0: Tweak path of quarterly endpoint.
    
    * FOIA-0: Overall fields for quarterly reports.
    
    * FOIA-0: Shuffle order of fields.
    
    * FOIA-0: Fiscal year instead of Year.
    
    * FOIA-0: Remove default values for quarterly fields.
    
    * FOIA-0: First draft of annual report placeholders.
    
    * FOIA-0: Clear all data button.
    
    * FOIA-0: Fix for clear data button.
    
    * FOIA-0: Quarterly migration progress.
    
    * FOIA-0: Temp db info for migration.
    
    * FOIA-0: Composer update 2021-08-11.
    
    * FOIA-0: Revert composer change.
    
    * FOIA-0: Patch for migrating paragraphs.
    
    * FOIA-0: Migration of quarterly reports for 2020.
    
    * FOIA-0: Actual db settings for Acquia.
    
    * FOIA-0: Do 2021 instead of 2020.
    
    * FOIA-0: Properly limit to 2021.
    
    * FOIA-0: More component fixes.
    
    * FOIA-0: More agency abbrev fixes.
    
    * FOIA-0: Another composer update.
    
    * FOIA-0: Remove db credentials for migration.
    
    * FOIA-0: Quarterly report listing for agency managers.
    
    * FOIA-0: Upgrade to dev version of Rules to fix block visibility issues.
    
    * FOIA-0: Limit quarterly block to user page.

---
+++ docroot/modules/custom/foia_quarterly_data_report/js/foia-change-report-agency.js
@@ -0,0 +1,82 @@
+/**
+ * @file
+ */
+
+(function ($, drupalSettings) {
+  Drupal.behaviors.foia_change_report_agency = {
+    attach: function attach() {
+      this.triggerNodeRefreshOnUpdate();
+      this.addPopulateComponentsButton();
+    },
+
+    /**
+     * Triggers the change.agency event which is listened for by the form element's ajax handler.
+     *
+     * The change event doesn't fire for the autocomplete field in IE11.  To work around this,
+     * this method listens for the blur event on the field, checks if the field value has
+     * changed, and triggers a refresh in all browsers if it has.
+     *
+     * @see foia_annual_data_report_ajax_existing_node()
+     * @see foia_annual_data_report_ajax_new_node()
+     */
+    triggerNodeRefreshOnUpdate: function () {
+
+      drupalSettings.foiaReportAgencyInitialValue = $('#edit-field-agency-0-target-id').val();
+
+      $('#edit-field-agency-0-target-id').once('foia-trigger-agency-change').blur(function (event) {
+        if ($(this).val() !== drupalSettings.foiaReportAgencyInitialValue) {
+          $('#edit-field-agency-0-target-id').trigger('change.agency');
+        }
+      });
+    },
+
+    addPopulateComponentsButton: function() {
+      var fieldWrapperSelector = '#edit-field-quarterly-component-data-wrapper',
+          addMoreSelector = 'input[name="field_quarterly_component_data_quarterly_component_data_add_more"]',
+          existingComponentSelector = '#edit-field-quarterly-component-data-wrapper tbody tr',
+          checkedComponentSelector = '#edit-field-agency-components input:checked',
+          componentDropdownSelector = '#edit-field-quarterly-component-data-wrapper table tr:last-child .field--name-field-agency-component select';
+      $(fieldWrapperSelector).once('foia-add-populate-button').each(function() {
+        $(this).prepend('<div class="description">Use this button when starting a new report, to quickly add placeholders for all of the components that you have selected in the checkboxes above.</div>');
+        var $button = $('<button class="button component-placeholder-button">Add placeholders for component data below</button>');
+        $(this).prepend($button);
+        $button.click(function(evt) {
+          evt.preventDefault();
+          if ($(existingComponentSelector).length > 0) {
+            alert('Placeholders cannot be added while there are existing entries. Please remove all entries and try again.');
+            return;
+          }
+          var $components = $(checkedComponentSelector),
+              numComponents = $components.length;
+          if (numComponents === 0) {
+            alert('First select the components you want using the checkboxes above.');
+            return;
+          }
+          var currentComponent = 0;
+          function clickAddMoreButton() {
+            $(addMoreSelector).trigger('mousedown');
+          }
+          function populateNextComponent() {
+            var componentNodeId = $components.eq(currentComponent).val();
+            $(componentDropdownSelector).val(componentNodeId);
+
+            currentComponent += 1;
+            if (currentComponent < numComponents) {
+              clickAddMoreButton();
+            }
+            else {
+              alert('Finished adding placeholders.');
+            }
+          }
+          $(document).on('ajaxStop', function() {
+            if (currentComponent < numComponents) {
+              populateNextComponent(currentComponent);
+            }
+          });
+          // Kick things off with the first click.
+          clickAddMoreButton();
+        });
+      });
+    }
+  }
+})(jQuery, drupalSettings);
