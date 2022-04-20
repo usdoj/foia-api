cdc65399a850710087917011c1c6873a891289cd
Amir Meshkin <amir.meshkin@gmail.com>
2022-04-20 11:46:19 -0400

    FOIA-87103: Pushing up quarterl agency component fix for review.

--- docroot/modules/custom/foia_quarterly_data_report/js/foia-change-report-agency.js
+++ docroot/modules/custom/foia_quarterly_data_report/js/foia-change-report-agency.js
@@ -3,12 +3,29 @@
  */
 
 (function ($, drupalSettings) {
+  console.log("drupalSettings");
+  let fieldAgency = $('#edit-field-agency-0-target-id');
   Drupal.behaviors.foia_change_report_agency = {
     attach: function attach() {
       this.triggerNodeRefreshOnUpdate();
       this.addPopulateComponentsButton();
-    },
+      this.modalButton();
 
+    },
+    /**
+     * Clears out Agency field on modal close
+     * @see foia_quarterly_data_report_create_node()
+     */
+    modalButton: function () {
+      $( ".agency-dialog" ).on( "dialogbeforeclose", function( e, ui ) {
+        fieldAgency.val('');
+      });
+      $('.agency-back').click(function(e) {
+        fieldAgency.val('');
+        $('.ui-icon-closethick').trigger('click');
+        e.preventDefault();
+      });
+    },
     /**
      * Triggers the change.agency event which is listened for by the form element's ajax handler.
      *
@@ -20,22 +37,25 @@
      * @see foia_annual_data_report_ajax_new_node()
      */
     triggerNodeRefreshOnUpdate: function () {
+      console.log("triggerNodeRefreshOnUpdate");
+      drupalSettings.foiaReportAgencyInitialValue = fieldAgency.val();
 
-      drupalSettings.foiaReportAgencyInitialValue = $('#edit-field-agency-0-target-id').val();
-
-      $('#edit-field-agency-0-target-id').once('foia-trigger-agency-change').blur(function (event) {
+      fieldAgency.once('foia-trigger-agency-change').blur(function (event) {
+        console.log("#edit-field-agency-0-target-id value", $(this).val())
         if ($(this).val() !== drupalSettings.foiaReportAgencyInitialValue) {
-          $('#edit-field-agency-0-target-id').trigger('change.agency');
+          console.log("triggerNodeRefreshOnUpdate trigger: change.agency")
+          fieldAgency.trigger('change.agency');
         }
       });
     },
 
     addPopulateComponentsButton: function() {
+      console.log("addPopulateComponentsButton")
       var fieldWrapperSelector = '#edit-field-quarterly-component-data-wrapper',
-          addMoreSelector = 'input[name="field_quarterly_component_data_quarterly_component_data_add_more"]',
-          existingComponentSelector = '#edit-field-quarterly-component-data-wrapper tbody tr',
-          checkedComponentSelector = '#edit-field-agency-components input:checked',
-          componentDropdownSelector = '#edit-field-quarterly-component-data-wrapper table tr:last-child .field--name-field-agency-component select';
+        addMoreSelector = 'input[name="field_quarterly_component_data_quarterly_component_data_add_more"]',
+        existingComponentSelector = '#edit-field-quarterly-component-data-wrapper tbody tr',
+        checkedComponentSelector = '#edit-field-agency-components input:checked',
+        componentDropdownSelector = '#edit-field-quarterly-component-data-wrapper table tr:last-child .field--name-field-agency-component select';
       $(fieldWrapperSelector).once('foia-add-populate-button').each(function() {
         $(this).prepend('<div class="description">Use this button when starting a new report, to quickly add placeholders for all of the components that you have selected in the checkboxes above.</div>');
         var $button = $('<button class="button component-placeholder-button">Add placeholders for component data below</button>');
@@ -43,11 +63,12 @@
         $button.click(function(evt) {
           evt.preventDefault();
           if ($(existingComponentSelector).length > 0) {
+            //$(existingComponentSelector).remove();
             alert('Placeholders cannot be added while there are existing entries. Please remove all entries and try again.');
             return;
           }
           var $components = $(checkedComponentSelector),
-              numComponents = $components.length;
+            numComponents = $components.length;
           if (numComponents === 0) {
             alert('First select the components you want using the checkboxes above.');
             return;
