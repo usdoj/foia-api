a417b593b90620ced509e2611f9a8565e3f5d5ff
Brock Fanning <brockfanning@gmail.com>
2022-11-22 21:37:21 +0000

    FOIA-140549: Linting fixes in class methods.

--- docroot/modules/custom/foia_request/src/Form/FoiaRequestPurgeForm.php
+++ docroot/modules/custom/foia_request/src/Form/FoiaRequestPurgeForm.php
@@ -12,22 +12,31 @@ use Drupal\foia_request\Entity\FoiaRequest;
  */
 class FoiaRequestPurgeForm extends FormBase {
 
+  /**
+   * Get the form id.
+   */
   public function getFormId() {
     return 'foia_request_purge_form';
   }
 
+  /**
+   * Build the form to purge failed requests.
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $requests = $this->queryRequests();
     $form['info']['#markup'] = '<p>There are currently ' . count($requests) . ' failed requests dated more than 2 weeks ago.</p>';
     $form['actions']['#type'] = 'actions';
-    $form['actions']['submit'] = array(
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Delete these requests permanently'),
       '#button_type' => 'primary',
-    );
+    ];
     return $form;
   }
 
+  /**
+   * Internal method to query failed requests 2 weeks or older.
+   */
   private function queryRequests() {
     $cutoff_date = strtotime('-2 weeks');
     return \Drupal::entityQuery('foia_request')
@@ -36,6 +45,9 @@ class FoiaRequestPurgeForm extends FormBase {
       ->execute();
   }
 
+  /**
+   * Submit the form and purge the requests.
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $requests = $this->queryRequests();
     $total_deleted = 0;
