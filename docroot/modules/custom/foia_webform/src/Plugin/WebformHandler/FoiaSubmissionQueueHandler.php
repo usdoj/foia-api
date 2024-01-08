b02dccfad414ffd080b609601fec4b82efd08993
Amir Meshkin <amir.meshkin@gmail.com>
2024-01-08 15:30:14 -0500

    FOIA-453: New field field_webform_submission_website in foia_request stores true false depending on hard coded production API key.

--- docroot/modules/custom/foia_webform/src/Plugin/WebformHandler/FoiaSubmissionQueueHandler.php
+++ docroot/modules/custom/foia_webform/src/Plugin/WebformHandler/FoiaSubmissionQueueHandler.php
@@ -69,6 +69,7 @@ class FoiaSubmissionQueueHandler extends EmailWebformHandler {
   protected function createFoiaRequest(WebformSubmissionInterface $webformSubmission, NodeInterface $agencyComponent) {
     $foiaRequest = FoiaRequest::create([
       'field_webform_submission_id' => $webformSubmission->id(),
+      'field_webform_submission_website' => isset($_SERVER["HTTP_X_API_KEY"]) && $_SERVER["HTTP_X_API_KEY"] === 'mUPoczW5VDRQOvroK6srQIjEGc5xBP0KDHgE34fv' ? TRUE : FALSE,
       'field_agency_component' => [
         'target_id' => $agencyComponent->id(),
       ],
