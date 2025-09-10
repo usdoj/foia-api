d149c2bfd8ca0a8b8cfb82557612eea1670ad1ba
Brock Fanning <1319083+brockfanning@users.noreply.github.com>
2025-09-10 15:54:27 +0000

    FOIA-0: More debugging.

--- docroot/sites/default/settings/includes.settings.php
+++ docroot/sites/default/settings/includes.settings.php
@@ -13,9 +13,9 @@ use Acquia\Blt\Robo\Common\EnvironmentDetector;
  */
 
 if (EnvironmentDetector::isAhEnv()) {
-  die('This is Acquia');
   $ah_group = EnvironmentDetector::getAhGroup();
   $ah_env = EnvironmentDetector::getAhEnv();
+  die($ah_env);
   if ($ah_env != 'ide') {
     $additionalSettingsFiles = [
       '/mnt/gfs/home/' . $ah_group . '/' . $ah_env . '/secrets.settings.php',
